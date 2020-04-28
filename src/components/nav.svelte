657234b05a41d291a09869aeb6ad844f16ca4385
Ruben Teck <ruben.teck@outlook.com>
2020-04-28 21:39:09 +0200

    aaaa

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -43,9 +43,9 @@
 </style>
 
 <nav>
-	<!-- <ul>
+	<ul>
 		<li><a aria-current='{segment === undefined ? "page" : undefined}' href='.'>home</a></li>
 		<li><a aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a></li>
 		<li><a rel=prefetch aria-current='{segment === "blog" ? "page" : undefined}' href='blog'>blog</a></li>
-	</ul> -->
+	</ul>
 </nav>
